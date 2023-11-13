e1c6543b9f595a2b3476364e617c45e45460bbf0
AdriaRod <adriarodriguez34@gmail.com>
2023-11-13 13:23:51 +0100

    Added Splash

--- app/src/main/java/com/example/nicestart/Login.java
+++ app/src/main/java/com/example/nicestart/Login.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView mGirl = findViewById(R.id.chica);
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .into(mGirl);
 
     }
         public void openMain(View v) {
