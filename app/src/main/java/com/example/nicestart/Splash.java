dad53a92ae464084ee951afa8f7f81bfea055059
AdriaRod <adriarodriguez34@gmail.com>
2023-11-16 13:55:07 +0100

    added Lottie

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -1,7 +1,5 @@
 package com.example.nicestart;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -10,6 +8,8 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.airbnb.lottie.LottieAnimationView;
 import com.airbnb.lottie.LottieDrawable;
 import com.bumptech.glide.Glide;
@@ -17,35 +17,42 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
+
+    private LottieAnimationView animationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         openApp();
 
+        /*
         ImageView thunder = findViewById(R.id.logosplash);
-
-        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.prueba);
-        thunder.startAnimation(myanim);
+        Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.prueba);
+        thunder.startAnimation(myAnim);*/
 
         ImageView mSea = findViewById(R.id.backView);
 
-       Glide.with(this)
+        Glide.with(this)
                 .load("https://images.unsplash.com/photo-1565214975484-3cfa9e56f914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1482&q=80")
                 .transition(DrawableTransitionOptions.withCrossFade(100))
                 .centerCrop()
                 .diskCacheStrategy(DiskCacheStrategy.ALL) //Guarda la imagen en caché
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
                 .into(mSea);
+
+        animationView = findViewById(R.id.animationView);
+        animationView.setAnimation(R.raw.cubitos2);             //Nombre de la animacion en res/raw
+        animationView.setRepeatCount(LottieDrawable.INFINITE); //Que sea infinito
     }
+
 
     private void openApp() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash
-                        .this, Login.class);
+                Intent intent = new Intent(Splash.this, Login.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
