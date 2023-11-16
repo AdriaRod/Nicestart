5cbf13721c07f8959e12b01a0cfdfeed65144c37
AdriaRod <adriarodriguez34@gmail.com>
2023-11-16 12:34:13 +0100

    added Animations

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -6,7 +6,12 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
@@ -17,6 +22,12 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         openApp();
+
+        ImageView thunder = findViewById(R.id.logosplash);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.prueba);
+        thunder.startAnimation(myanim);
+
         ImageView mSea = findViewById(R.id.backView);
 
        Glide.with(this)
